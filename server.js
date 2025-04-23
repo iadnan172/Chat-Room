0922fff9ec71ef459013394a057e88ce27295297
iadnan172 <pathanadnan079@gmail.com>
2025-04-23 14:25:13 +0530

    add mysql pckages

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
 const { createAdapter } = require("@socket.io/redis-adapter");
 const { createClient } = require("redis"); // ✅ Yeh line fix hai
-// const mysql = require("mysql2");
+ const mysql = require("mysql2");
 const Redis = require('ioredis');
 require("dotenv").config();
 console.log("👉 REDIS_URL loaded from .env or Render:", process.env.REDIS_URL);
